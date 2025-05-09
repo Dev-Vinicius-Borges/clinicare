07d6072b8ff947e7b53b000be2082e7402402738
dev-vinicius-borges <dev.vinicius.borges@outlook.com>
2025-05-09 16:37:55 -0300

    Feat: Criando as rotas para as páginas criadas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:clini_care/components/RecuperarSenha/ReceberCodigo.dart';
+import 'package:clini_care/components/registro/RegistroDadosPessoais.dart';
+import 'package:clini_care/components/registro/RegistroEndereco.dart';
+import 'package:clini_care/components/registro/RegistroSeguranca.dart';
 import 'package:clini_care/pages/AgendamentosPage.dart';
 import 'package:clini_care/pages/ContaPage.dart';
 import 'package:clini_care/pages/IndexPage.dart';
 import 'package:clini_care/pages/LoginPage.dart';
+import 'package:clini_care/pages/recuperarSenha/AlterarSenhaPage.dart';
+import 'package:clini_care/pages/recuperarSenha/ReceberCodigoPage.dart';
 import 'package:clini_care/pages/recuperarSenha/RecuperarSenhaPage.dart';
 import 'package:clini_care/pages/registro/RegistroDadosPessoaisPage.dart';
+import 'package:clini_care/pages/registro/RegistroEnderecoPage.dart';
+import 'package:clini_care/pages/registro/RegistroSegurancaPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,8 +31,13 @@ class MyApp extends StatelessWidget {
       initialRoute: '/login',
       routes: {
         '/login': (context) => LoginPage(),
-        '/registro': (context) => RegistroPage(),
+        '/registro': (context) => RegistroDadosPessoaisPage(),
+        '/registro/dados_pessoais': (context) => RegistroDadosPessoaisPage(),
+        '/registro/endereco': (context) => RegistroEnderecoPage(),
+        '/registro/seguranca': (context) => RegistroSegurancaPage(),
         '/recuperar_senha': (context) => RecuperarSenhaPage(),
+        '/recuperar_senha/receber_codigo': (context) => ReceberCodigoPage(),
+        '/recuperar_senha/criar_nova_senha': (context) => AlterarSenhaPage(),
         '/inicio': (context) => IndexPage(),
         '/conta': (context) => ContaPage(),
         '/agendamentos': (context) => AgendamentosPage()
