dae2f2ea338b5a8b4f121020b922d461675dc7c5
= <=>
2025-05-21 05:12:34 -0300

    Feat (config): Configurando datas suportadas.

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,26 @@ import 'package:clini_care/pages/registro/RegistroEnderecoPage.dart';
 import 'package:clini_care/pages/registro/RegistroSegurancaPage.dart';
 import 'package:clini_care/server/session/configuracao.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
 
-void main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: "https://jxcahmhcsfdrnkcknnbs.supabase.co",
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4Y2FobWhjc2Zkcm5rY2tubmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTkwMTAsImV4cCI6MjA2MzE3NTAxMH0.8ry2-UYnSiz8tJfp6WDD8oTK-kfWYcQQBI_R6D-PXqU",
+      url: "https://jxcahmhcsfdrnkcknnbs.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4Y2FobWhjc2Zkcm5rY2tubmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTkwMTAsImV4cCI6MjA2MzE3NTAxMH0.8ry2-UYnSiz8tJfp6WDD8oTK-kfWYcQQBI_R6D-PXqU"
   );
 
   await initializeDateFormatting('pt_BR');
 
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => GerenciadorDeSessao()..loadSession(),
-      child: MyApp(),
-    ),
+      ChangeNotifierProvider(
+        create: (context) => GerenciadorDeSessao()..loadSession(),
+        child: MyApp(),
+      )
   );
 }
 
@@ -43,6 +43,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+        Locale('en', 'US'),
+      ],
+      locale: const Locale('pt', 'BR'),
       initialRoute: '/login',
       routes: {
         '/login': (context) => LoginPage(),
@@ -55,7 +65,7 @@ class MyApp extends StatelessWidget {
         '/recuperar_senha/criar_nova_senha': (context) => AlterarSenhaPage(),
         '/inicio': (context) => IndexPage(),
         '/conta': (context) => ContaPage(),
-        '/agendamentos': (context) => AgendamentosPage(),
+        '/agendamentos': (context) => AgendamentosPage()
       },
     );
   }
